0c7fcfb2ef120bde2841e4ae7afa38c6cec2beec
Askar Sariev <askar-64@mail.ru>
2022-11-21 19:04:43 +0300

    Create project

---
+++ src/main/java/ru/sariev/word_trainer_app/WordTrainerAppApplication.java
@@ -0,0 +1,13 @@
+package ru.sariev.word_trainer_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordTrainerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordTrainerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sariev/word_trainer_app/WordTrainerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sariev.word_trainer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordTrainerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

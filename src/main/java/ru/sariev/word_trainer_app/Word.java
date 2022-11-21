d41c07ff9bdab4f530a9402abdc4f8c5faa2d92e
Askar Sariev <askar-64@mail.ru>
2022-11-21 19:26:02 +0300

    Add Entity class

---
+++ src/main/java/ru/sariev/word_trainer_app/Word.java
@@ -0,0 +1,73 @@
+package ru.sariev.word_trainer_app;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Words")
+public class Word {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "original")
+    private String original;
+
+    @Column(name = "translation")
+    private String translation;
+
+    @Column(name = "status")
+    private boolean status;
+
+    public Word() {
+    }
+
+    public Word(String original, String translation, boolean status) {
+        this.original = original;
+        this.translation = translation;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOriginal() {
+        return original;
+    }
+
+    public void setOriginal(String original) {
+        this.original = original;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "id=" + id +
+                ", original='" + original + '\'' +
+                ", translation='" + translation + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}

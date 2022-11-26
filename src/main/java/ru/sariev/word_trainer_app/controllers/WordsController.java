56ace24f69ae99e9219c986379c33b61f5ae7821
Askar Sariev <askar-64@mail.ru>
2022-11-26 10:17:17 +0300

    Add NewWord, Edit, Trainer

--- src/main/java/ru/sariev/word_trainer_app/controllers/WordsController.java
+++ src/main/java/ru/sariev/word_trainer_app/controllers/WordsController.java
@@ -3,12 +3,12 @@ package ru.sariev.word_trainer_app.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.sariev.word_trainer_app.models.Word;
 import ru.sariev.word_trainer_app.services.WordsService;
 
 @Controller
-@RequestMapping("words")
+@RequestMapping("train")
 public class WordsController {
 
     private WordsService wordsService;
@@ -20,17 +20,65 @@ public class WordsController {
 
     @GetMapping("/main")
     public String selectAction() {
-        return "words/mainPage";
+        return "wordtrainer/mainPage";
     }
 
     @GetMapping("/allwords")
     public String showAllWords(Model model) {
         model.addAttribute("words", wordsService.getAllWords());
-        return "words/allWordsPage";
+        return "wordtrainer/allWordsPage";
+    }
+
+    @GetMapping("/new")
+    public String addNewWord(@ModelAttribute("word") Word word) {
+        return "wordtrainer/newWordPage";
+    }
+
+    @PostMapping("/allwords")
+    public String createWord(@ModelAttribute("word") Word word) {
+        wordsService.save(word);
+        return "redirect:allwords";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("word", wordsService.findOne(id));
+        return "wordtrainer/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("word") Word word, @PathVariable("id") int id) {
+        wordsService.update(id, word);
+        return "redirect:allwords";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteWord(@PathVariable("id") int id) {
+        wordsService.delete(id);
+        return "redirect:allwords";
     }
 
     @GetMapping("/trainer")
-    public String startTraining() {
-        return "words/trainerPage";
+    public String startTraining(Model model) {
+        model.addAttribute("word", wordsService.getRandomWord());
+        return "wordtrainer/trainerPage";
+    }
+
+    @GetMapping("/next")
+    public String nextWord(Model model) {
+        model.addAttribute("word", wordsService.getRandomWord());
+        return "redirect:trainer";
+    }
+
+    @GetMapping("/{id}/editTrain")
+    public String editTrain(Model model, @PathVariable("id") int id) {
+        model.addAttribute("word", wordsService.findOne(id));
+        return "wordtrainer/editTrain";
+    }
+
+    @PatchMapping("/{id}/update")
+    public String updateTrain(@ModelAttribute("word") Word word, @PathVariable("id") int id) {
+        wordsService.update(id, word);
+        return "wordtrainer/trainerPage";
     }
 }
--- src/main/java/ru/sariev/word_trainer_app/services/WordsService.java
+++ src/main/java/ru/sariev/word_trainer_app/services/WordsService.java
@@ -7,10 +7,12 @@ import ru.sariev.word_trainer_app.models.Word;
 import ru.sariev.word_trainer_app.repository.WordsRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 public class WordsService {
+    private int i = 0;
 
     private WordsRepository wordsRepository;
 
@@ -21,5 +23,36 @@ public class WordsService {
 
     public List<Word> getAllWords() {
         return wordsRepository.findAll();
+    }
+
+    public Word findOne(int id) {
+        Optional<Word> foundWord = wordsRepository.findById(id);
+        return foundWord.orElse(null);
+    }
+
+    @Transactional
+    public void save(Word word) {
+        wordsRepository.save(word);
+    }
+
+    @Transactional
+    public void update(int id, Word updatedWord) {
+        updatedWord.setId(id);
+        wordsRepository.save(updatedWord);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        wordsRepository.deleteById(id);
+    }
+
+    public Word getRandomWord() {
+        List<Word> list = getAllWords();
+
+        if (i == list.size()) {
+            i = 0;
+        }
+
+        return list.get(i++);
     }
 }

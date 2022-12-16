276e7dd246a22fee11d7a585d2a6e06541ae931f
Askar Sariev <askar-64@mail.ru>
2022-12-16 22:25:12 +0300

    Update SecurityConfig

--- src/main/java/ru/sariev/word_trainer_app/config/SecurityConfig.java
+++ src/main/java/ru/sariev/word_trainer_app/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package ru.sariev.word_trainer_app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {

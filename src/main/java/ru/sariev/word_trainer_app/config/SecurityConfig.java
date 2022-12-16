9a42787b2928df73f32b0dfbbd920cfc9bcf3a41
Askar Sariev <askar-64@mail.ru>
2022-12-16 18:47:07 +0300

    Add inmemoryauthentication

---
+++ src/main/java/ru/sariev/word_trainer_app/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package ru.sariev.word_trainer_app.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/train/main", "/train/allwords", "/train/trainer", "/train/next", "/train/{id}/showTranslation")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/train/{id}/edit", "/train/{id}", "/train/{id}/editWordOnTraining", "/train/{id}/updateWordOnTraining")
+                .hasRole("ADMIN")
+                .and()
+                .formLogin().permitAll()
+                .defaultSuccessUrl("/train/main")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("user").password("user").roles("USER"))
+                .withUser(userBuilder.username("admin").password("admin").roles("ADMIN"));
+    }
+}
